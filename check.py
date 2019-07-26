1917b23f568a9fe0b826e727584bfa0868a571f0
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-26 16:51:49 -0400

    add basic result check functionality

---
+++ check.py
@@ -0,0 +1,23 @@
+with open("known_dups") as fp:
+    known_dups = set()
+    for line in fp.readlines():
+        id0, id1 = map(int, line.strip().split(" "))
+        known_dups.add((min(id0, id1), max(id0, id1)))
+
+with open("discovered_dups") as fp:
+    discovered_dups = set()
+    scores = {}
+    for line in fp.readlines():
+        id0, id1, score = map(float, line.strip().split(" "))
+        dup = (min(id0, id1), max(id0, id1))
+        discovered_dups.add(dup)
+        scores[dup] = score
+
+found = 0
+for known in known_dups:
+    if known in discovered_dups:
+        print(known, scores[known])
+        found += 1
+
+print("known", "discovered", "found", "hit_rate")
+print(len(known_dups), len(discovered_dups), found, found / len(known_dups))
