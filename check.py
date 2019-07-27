acc207e1fb30cd355595406d10f9bb4ce11a527a
Matthew Farrellee <matt@cs.wisc.edu>
2019-07-27 05:48:17 -0400

    use testset with positive and negative examples, output standard stats

--- check.py
+++ check.py
@@ -1,9 +1,14 @@
-with open("known_dups") as fp:
+with open("testset") as fp:
     known_dups = set()
+    known_nondups = set()
     for line in fp.readlines():
-        id0, id1 = map(int, line.strip().split(" "))
+        id0, id1, score = map(float, line.strip().split(" "))
         if id0 != id1:
-            known_dups.add((min(id0, id1), max(id0, id1)))
+            pair = (min(id0, id1), max(id0, id1))
+            if score == 1:
+                known_dups.add(pair)
+            elif score == 0:
+                known_nondups.add(pair)
 
 with open("discovered_dups") as fp:
     discovered_dups = set()
@@ -14,11 +19,30 @@ with open("discovered_dups") as fp:
         discovered_dups.add(dup)
         scores[dup] = score
 
-found = 0
-for known in known_dups:
-    if known in discovered_dups:
-        print(known, scores[known])
-        found += 1
+POSITIVES = len(known_dups)
+NEGATIVES = len(known_nondups)
+TOTAL = POSITIVES + NEGATIVES
+PREDICTED = len(discovered_dups)
 
-print("known", "discovered", "found", "hit_rate")
-print(len(known_dups), len(discovered_dups), found, found / len(known_dups))
+TN = len(known_nondups.difference(discovered_dups))
+FP = len(known_nondups.intersection(discovered_dups))
+FN = len(known_dups.difference(discovered_dups))
+TP = len(known_dups.intersection(discovered_dups))
+
+def pct(n):
+    return "%i%%" % (n * 100,)
+
+print("known dups:", POSITIVES)
+print("known non-dups:", NEGATIVES)
+print("discovered dups:", PREDICTED)
+print("TP:", TP)
+print("FN:", FN)
+print("TN:", TN)
+print("FP:", FP)
+print("accuracy:", pct((TP + TN) / TOTAL))
+print("misclassification rate:", pct((FP + FN) / TOTAL))
+print("true positive rate | sensitivity | recall:", pct(TP / POSITIVES))
+print("false positive rate:", pct(FP / NEGATIVES))
+print("true negative rate | specificity:", pct(TN / NEGATIVES))
+print("precision:", pct(TP / PREDICTED))
+print("prevalence:", pct(POSITIVES / TOTAL))
